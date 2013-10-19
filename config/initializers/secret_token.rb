fc3d9735f2f7e32c27fc82a30b35b5963d0e7fc6
rohan1020 <rohanraja9@gmail.com>
2013-10-19 11:37:44 +0530

    Initial Tempelate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tnp::Application.config.secret_key_base = '65ede549aaeb32f96aa3c2f8bd1ed065a7d3428b1c5fc4168705b62c95abfa6d0a1d3753cd212e98fd2d337d162ff38797fac6f6c64e82a6a3f32c4bf3f0113c'
